2e374c74b566f23204b7d9b275c97b63aa554ddd
2daeeun <ghostintheme@naver.com>
2023-06-19 16:15:39 +0900

    심볼릭 링크 생성 스크립트

---
+++ symbolic_link.sh
@@ -0,0 +1,19 @@
+sudo rm -rf ~/.config/autorandr
+sudo rm -rf ~/.config/htop
+sudo rm -rf ~/.config/i3
+sudo rm -rf ~/.config/neofetch
+sudo rm -rf ~/.config/nvim
+sudo rm -rf ~/.config/terminator
+sudo rm -rf ~/.p10k.zsh
+sudo rm -rf ~/.xprofile
+sudo rm -rf ~/.zshrc
+
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/autorandr ~/.config/
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/htop ~/.config/
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/i3 ~/.config/
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/neofetch ~/.config/
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/nvim ~/.config/
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/terminator ~/.config/
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.p10k.zsh ~/
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.xprofile ~/
+sudo ln -sf ~/Documents/github/my_endeavouros/home/.zshrc ~/
