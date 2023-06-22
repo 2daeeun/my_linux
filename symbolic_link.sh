822044f7b59f5afe62e807eec68b119c3bc25939
2daeeun <ghostintheme@naver.com>
2023-06-23 06:54:50 +0900

    fix:심볼릭 링크 생성 스크립트
    디렉토리 경로명 오류 및 일부 심볼릭 링크 생성 추가

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -1,19 +1,25 @@
 sudo rm -rf ~/.config/autorandr
 sudo rm -rf ~/.config/htop
 sudo rm -rf ~/.config/i3
+sudo rm -rf ~/.config/i3status
 sudo rm -rf ~/.config/neofetch
 sudo rm -rf ~/.config/nvim
+sudo rm -rf ~/.config/nvim_old
+sudo rm -rf ~/.config/parcellite
 sudo rm -rf ~/.config/terminator
 sudo rm -rf ~/.p10k.zsh
 sudo rm -rf ~/.xprofile
 sudo rm -rf ~/.zshrc
 
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/autorandr ~/.config/
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/htop ~/.config/
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/i3 ~/.config/
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/neofetch ~/.config/
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/nvim ~/.config/
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.config/terminator ~/.config/
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.p10k.zsh ~/
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.xprofile ~/
-sudo ln -sf ~/Documents/github/my_endeavouros/home/.zshrc ~/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/autorandr ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/htop ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/i3 ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/i3status ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/neofetch ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/nvim ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/nvim_old ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/parcellite ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/terminator ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.p10k.zsh ~/
+sudo ln -sf ~/Documents/github/my_linux/home/.xprofile ~/
+sudo ln -sf ~/Documents/github/my_linux/home/.zshrc ~/
