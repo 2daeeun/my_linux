4f83766c2b3d0850d7fef9e4f20d47d5c6efaa02
2daeeun <ghostintheme@naver.com>
2023-06-23 20:10:59 +0900

    [추가] gtk 심볼릭 링크 추가

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -1,4 +1,5 @@
 sudo rm -rf ~/.config/autorandr
+sudo rm -rf ~/.config/gtk-3.0
 sudo rm -rf ~/.config/htop
 sudo rm -rf ~/.config/i3
 sudo rm -rf ~/.config/i3status
@@ -12,6 +13,7 @@ sudo rm -rf ~/.xprofile
 sudo rm -rf ~/.zshrc
 
 sudo ln -sf ~/Documents/github/my_linux/home/.config/autorandr ~/.config/
+sudo ln -sf ~/Documents/github/my_linux/home/.config/gtk-3.0 ~/.config/
 sudo ln -sf ~/Documents/github/my_linux/home/.config/htop ~/.config/
 sudo ln -sf ~/Documents/github/my_linux/home/.config/i3 ~/.config/
 sudo ln -sf ~/Documents/github/my_linux/home/.config/i3status ~/.config/
