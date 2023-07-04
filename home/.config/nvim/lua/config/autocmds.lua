f6f546aaf446afb22b2647362f4d22c187d58aa8
2daeeun <ghostintheme@naver.com>
2023-07-05 01:38:48 +0900

    fix : Enable autoformat

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -2,9 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 -- Disable autoformat for lua files
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "c", "cpp"},
-  callback = function()
-    vim.b.autoformat = false
-  end,
-})
+
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   pattern = { "c", "cpp"},
+--   callback = function()
+--     vim.b.autoformat = false
+--   end,
+-- })
