0c55f066080b7c22417c0990aeae97f0afb815a7
2daeeun <ghostintheme@naver.com>
2023-07-06 01:11:14 +0900

    [수정] 탭 간격 4칸에서 2칸으로 수정

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+-- vim.opt.shiftwidth = 4
+-- vim.opt.tabstop = 4
 vim.opt.relativenumber = false
