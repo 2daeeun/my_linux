945ee37db50b72574c152fd6b832dec100e9b25d
2daeeun <ghostintheme@naver.com>
2023-07-10 00:24:29 +0900

    fix : NotifyBackground 에러 추가 해결

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -4,3 +4,9 @@ require("config.lazy")
 -- vim.opt.shiftwidth = 4
 -- vim.opt.tabstop = 4
 vim.opt.relativenumber = false
+
+vim.notify = require("notify")
+require("notify").setup({
+  background_colour = "#000000",
+  stages = "fade",
+})
