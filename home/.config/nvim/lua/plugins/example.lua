a935357b1a95efa5a370fbcc3b773cd0be7000ef
2daeeun <ghostintheme@naver.com>
2023-08-08 14:49:12 +0900

    [추가] dart 언어 서비스 추가

--- home/.config/nvim/lua/plugins/example.lua
+++ home/.config/nvim/lua/plugins/example.lua
@@ -87,6 +87,7 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+        dartls = {},
       },
     },
   },
