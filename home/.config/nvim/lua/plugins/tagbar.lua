58230391f9c8e84dbf673e39d10921909e056483
2daeeun <leedaeeun@yahoo.com>
2023-12-20 11:26:38 +0900

    fix : tagbar 플러그인 옵션들 추가

--- home/.config/nvim/lua/plugins/tagbar.lua
+++ home/.config/nvim/lua/plugins/tagbar.lua
@@ -1,3 +1,18 @@
 return {
-  "majutsushi/tagbar",
+  "majutsushi/tagbar", -- shows classes, methods inside file
+  config = function()
+    vim.g.tagbar_iconchars = { "▸", "▾" }
+
+    -- Disable use_cache when installed via snap.
+    -- Tagbar in this setup cannot acces /tmp directory.
+    -- Source: https://github.com/preservim/tagbar/issues/714
+    vim.g["tagbar_use_cache"] = 0
+
+    -- set keymaps
+    local keymap = vim.keymap
+
+    -- universal-ctags are installed via repo clone
+    -- Source: https://askubuntu.com/a/836521/1081442
+    keymap.set("n", "<F8>", "<cmd>TagbarToggle<cr>", { desc = "Toggle Tagbar" })
+  end,
 }
