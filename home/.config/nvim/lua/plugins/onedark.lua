a08c421b54a1ad04380475531c870e3eb0214486
2daeeun <leedaeeun@yahoo.com>
2024-02-24 22:03:27 +0900

    [수정] nvim 테마 변경

--- home/.config/nvim/lua/plugins/onedark.lua
+++ home/.config/nvim/lua/plugins/onedark.lua
@@ -2,16 +2,48 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "onedark_dark",
     },
   },
 
+  -- 기존에 사용했던 테마
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   opts = {
+  --     style = "deep",
+  --     transparent = true,
+  --     background_colour = "#000000",
+  --   },
+  -- },
+
   {
-    "navarasu/onedark.nvim",
-    opts = {
-      style = "deep",
-      transparent = true,
-      background_colour = "#000000",
-    },
+    "olimorris/onedarkpro.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedarkpro").setup({
+        colors = {
+          bg = "#000000",
+          fg = "#abb2bf",
+          red = "#ef596f",
+          orange = "#d19a66",
+          yellow = "#e5c07b",
+          green = "#89ca78",
+          cyan = "#2bbac5",
+          blue = "#61afef",
+          purple = "#d55fde",
+          white = "#abb2bf",
+          black = "#000000",
+          gray = "#434852",
+          highlight = "#e2be7d",
+          comment = "#7f848e",
+          none = "NONE",
+        },
+        options = {
+          transparency = true,
+        },
+      })
+      vim.cmd("colorscheme onedark_dark")
+    end,
   },
 }
