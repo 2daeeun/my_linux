789514f5751d3b6258d4014dacfd3cea1c8a2157
2daeeun <leedaeeun@yahoo.com>
2024-02-26 22:19:14 +0900

    [테스트 중] 1_install_chrome 자동화 스크립트 v1

--- 1_install_chrome.sh
+++ 1_install_chrome.sh
@@ -1,56 +1,58 @@
-# !/bin/sh
-# Text to ASCII ART Generator
-# URL : https://patorjk.com/software/taag
-# ASCII Art Font : ANSI Shadow
-# ASCII More Opts(Format the output) : Echo commands
+# !/bin/bash
 
-# ----- Create Directory -----
+# Create default directory
 mkdir -p ~/Documents
 mkdir -p ~/Downloads
 mkdir -p ~/Videos
 
-echo "Are you using Debian or Arch?"
-read -p "(Debian is D, Arch is A) <D/A> " prompt
-if [[ $prompt == "D" || $prompt == "d" ]]; then
-
-	# ----- Debian Update & Upgrade -----
+# Check if the OS is Debian or Ubuntu
+if [ -x "$(command -v apt-get)" ]; then
+	# apt update && apt upgrade
 	sudo apt-get update
 	sudo apt-get upgrade -y
 	sudo apt autoremove -y
 	sudo apt autoclean -y
 	sudo apt install wget -y
 
-	# ----- Install Chrome -----
+	# Install Chrome
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 	sudo apt install python3-apt -y
 	sudo apt install ./google-chrome-stable_current_amd64.deb -y
 	sudo rm -rf google-chrome-stable_current_amd64
 
-	# Fin
-	sudo apt update
-	sudo apt upgrade -y
-	sudo apt autoremove -y
-	sudo apt autoclean -y
+	exit 0
 
-elif [[ $prompt == "A" || $prompt == "a" ]]; then
-
-	# ----- Arch Update & Upgrade -----
-	yes | sudo pacman -Sy
-	yes | sudo pacman -S linux-lts linux-lts-headers
-	sudo grub-mkconfig -o /boot/grub/grub.cfg
-	yes | sudo pacman -S archlinux-keyring
-	sudo pacman -Syu
-
-	# ----- Install Chrome -----
-	yes | sudo pacman -Sy --needed base-devel git
-	git clone https://aur.archlinux.org/google-chrome.git
-	cd google-chrome
-	makepkg -is
-
-
-else
-	echo "other"
 fi
 
-# Exit the script
-exit 0
+# Check if the OS is EndeavourOS or Arch
+if [ -x "$(command -v pacman)" ]; then
+	# Install LTS kernel
+	sudo pacman -Sy
+	sudo pacman -S --noconfirm git
+	sudo pacman -S --noconfirm linux-lts linux-lts-headers
+
+	# Set linux-lts as the default kernel
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+	# Install archlinux-keyring
+	sudo pacman -S --noconfirm archlinux-keyring
+	sudo pacman -Syu --noconfirm
+
+	# Check if yay is installed
+	if ! command -v yay &>/dev/null; then
+		# Install yay
+		git clone https://aur.archlinux.org/yay.git
+		cd yay
+		makepkg -si
+		cd ..
+		rm -rf yay
+	fi
+
+	# Install Chrome
+	yay -S --noconfirm google-chrome
+
+	exit 0
+fi
+
+echo "error"
+exit 1
