107b858619772d8b9574562ba48c58e73c3c7a5a
2daeeun <leedaeeun@yahoo.com>
2024-03-02 20:34:57 +0000

    [테스트 중] 1_install_chrome 자동화 스크립트 v4

--- 1_install_chrome_and_kernel_update.sh
+++ 1_install_chrome_and_kernel_update.sh
@@ -7,7 +7,9 @@ mkdir -p ~/Videos
 
 # Check if the OS is Debian or Ubuntu
 if [ -x "$(command -v apt-get)" ]; then
-	# apt update && apt upgrade
+  # apt update && apt upgrade
+  echo -e "\n\n\n\n\napt update && apt upgrade\n\n\n\n\n"
+  sleep 5
 	sudo apt-get update
 	sudo apt-get upgrade -y
 	sudo apt autoremove -y
@@ -15,10 +17,12 @@ if [ -x "$(command -v apt-get)" ]; then
 	sudo apt install wget -y
 
 	# Install Chrome
+  echo -e "\n\n\n\n\nInstall Chrome\n\n\n\n\n"
+  sleep 5
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 	sudo apt install python3-apt -y
 	sudo apt install ./google-chrome-stable_current_amd64.deb -y
-	sudo rm -rf google-chrome-stable_current_amd64
+	sudo rm -rf google-chrome-stable_current_amd64.deb
 
 	exit 0
 
@@ -27,20 +31,28 @@ fi
 # Check if the OS is EndeavourOS or Arch
 if [ -x "$(command -v pacman)" ]; then
 	# Install LTS kernel
+  echo -e "\n\n\n\n\nInstall LTS kernel\n\n\n\n\n"
+  sleep 5
 	sudo pacman -Sy
 	sudo pacman -S --noconfirm git
 	sudo pacman -S --noconfirm linux-lts linux-lts-headers
 
 	# Set linux-lts as the default kernel
+  echo -e "\n\n\n\n\nSet linux-lts as the default kernel\n\n\n\n\n"
+  sleep 5
 	sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 	# Install archlinux-keyring
+  echo -e "\n\n\n\n\nInstall archlinux-keyring\n\n\n\n\n"
+  sleep 5
 	sudo pacman -S --noconfirm archlinux-keyring
 	sudo pacman -Syu --noconfirm
 
 	# Check if yay is installed
-	if ! command -v yay &>/dev/null; then
+  if ! command -v yay &>/dev/null; then
 		# Install yay
+    echo -e "\n\n\n\n\nInstall yay\n\n\n\n\n"
+    sleep 5
 		git clone https://aur.archlinux.org/yay.git
 		cd yay
 		makepkg -si
@@ -54,5 +66,5 @@ if [ -x "$(command -v pacman)" ]; then
 	exit 0
 fi
 
-echo "error"
-exit 1
+# Exit the script
+exit 0
