653d63503d7a9e1b20efecce92af5df3bae3704c
2daeeun <leedaeeun@yahoo.com>
2024-06-10 02:29:16 +0900

    [테스트 중] 3_install_zsh 자동화 스크립트 v0.1

--- 3_install_zsh.sh
+++ 3_install_zsh.sh
@@ -1,107 +1,145 @@
 # !/bin/sh
-# Text to ASCII ART Generator
-# URL : https://patorjk.com/software/taag
-# ASCII Art Font : ANSI Shadow
-# ASCII More Opts(Format the output) : Echo commands
 
-echo "███████╗███████╗██╗  ██╗    ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     "
-echo "╚══███╔╝██╔════╝██║  ██║    ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     "
-echo "  ███╔╝ ███████╗███████║    ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     "
-echo " ███╔╝  ╚════██║██╔══██║    ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     "
-echo "███████╗███████║██║  ██║    ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗"
-echo "╚══════╝╚══════╝╚═╝  ╚═╝    ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝"
-echo "                                                                                  "
-echo "                                                                                  "
-
-# ----- Create Directory -----
+# Create default directory
 mkdir -p ~/Documents
 mkdir -p ~/Downloads
 mkdir -p ~/Videos
 
-echo "Are you using Debian or Arch?"
-read -p "(Debian is D, Arch is A) <D/A> " prompt
-if [[ $prompt == "D" || $prompt == "d" ]]; then
+# Install ZSH Plugins Function
+install_zsh_plugins() {
+	echo -e "\n\n\n\n\n"
+	echo -e "┌─────Install ZSH Plugins─────┐"
+	echo -e "│ * Powerlevel10k 설치        │"
+	echo -e "│ * zsh 구문 강조 플러그인    │"
+	echo -e "│ * zsh 자동 제안 플러그인    │"
+	echo -e "└─────────────────────────────┘"
+	echo -e "\n\n\n\n\n"
+	sleep 5
+	echo -e "Powerlevel10k 설치"
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+	echo -e "\n"
+	echo -e "zsh 구문 강조 플러그인"
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	echo -e "\n"
+	echo -e "zsh 자동 제안 플러그인"
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+}
 
-	# ----- Debian Update & Upgrade -----
+# Install vim-plug
+echo -e "\n\n\n\n\nInstall vim-plug\n\n\n\n\n"
+sleep 5
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Check if the OS is Debian or Ubuntu
+if [ -x "$(command -v apt-get)" ]; then
+	# apt update && apt upgrade
+	echo -e "\n\n\n\n\napt update && apt upgrade\n\n\n\n\n"
+	sleep 5
 	sudo apt-get update
 	sudo apt-get upgrade -y
 	sudo apt autoremove -y
 	sudo apt autoclean -y
-	sudo apt install wget -y
-	sudo apt install curl -y
+	sudo apt install wget curl -y
 
-	# ----- Install ZSH -----
+	# Install ZSH
+	echo -e "\n\n\n\n\nInstall ZSH\n\n\n\n\n"
+	sleep 5
 	sudo apt-get install zsh -y
-	# ----- Install Oh-My-Zsh -----
+
+	# Install Oh-My-Zsh
+	echo -e "\n\n\n\n\nInstall Oh-My-Zsh\n\n\n\n\n"
+	sleep 5
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	# ----- Change Default Shell -----
+
+	# Change Default Shell
+	echo -e "\n\n\n\n\nChange Default Shell\n\n\n\n\n"
+	sleep 5
 	sudo chsh -s /usr/bin/zsh
 
-	# oh-my-zsh 폰트 깨짐 방지
+	# prompt and statusline utility (oh-my-zsh 폰트 깨짐 방지)
+	echo -e "\n\n\n\n\nprompt and statusline utility (oh-my-zsh 폰트 깨짐 방지)\n\n\n\n\n"
+	sleep 5
 	sudo apt-get install -y fonts-powerline
 
-	# Node.Js 최신 버전 설치
+	# Install Node.js
+	echo -e "\n\n\n\n\nInstall Node.js\n\n\n\n\n"
+	sleep 5
 	curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 	sudo apt update
 	sudo apt install nodejs -y
 
-	# yarn 최신 버전 설치
+	# Install yarn
+	echo -e "\n\n\n\n\nInstall yarn\n\n\n\n\n"
+	sleep 5
 	curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 	echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 	sudo apt update
 	sudo apt install yarn -y
 
-	# Nerd Fonts Install
+	# Install Nerd Fonts
+	echo -e "\n\n\n\n\nInstall Nerd Fonts\n\n\n\n\n"
+	sleep 5
 	sudo wget -P /usr/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf
 	sudo fc-cache -f -v
 
+	# Run install_zsh_plugins Function
+	install_zsh_plugins
 	# Fin
+	echo -e "\n\n\n\n\nFin\n\n\n\n\n"
+	sleep 5
 	sudo apt update
 	sudo apt upgrade -y
 	sudo apt autoremove -y
 	sudo apt autoclean -y
 
-elif [[ $prompt == "A" || $prompt == "a" ]]; then
+	exit 0
 
-	# ----- Arch Update & Upgrade -----
-	yes | sudo pacman -Sy
-	yes | sudo pacman -S wget
-	yes | sudo pacman -S curl
+fi
 
-	# ----- zsh 설치 -----
+# Check if the OS is EndeavourOS or Arch
+if [ -x "$(command -v pacman)" ]; then
+	# Arch Update & Upgrade
+	echo -e "\n\n\n\n\nArch Update & Upgrade\n\n\n\n\n"
+	sleep 5
+	sudo pacman -Sy
+	yes | sudo pacman -S wget curl
+
+	# Install ZSH
+	echo -e "\n\n\n\n\nInstall ZSH\n\n\n\n\n"
+	sleep 5
 	yes | sudo pacman -S zsh
-	# ----- oh-my-zsh 설치 -----
+
+	# Install Oh-My-Zsh
+	echo -e "\n\n\n\n\nInstall Oh-My-Zsh\n\n\n\n\n"
+	sleep 5
 	wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-	# ----- zsh를 기본 쉘로 설정 -----
+
+	# Change Default Shell
+	echo -e "\n\n\n\n\nChange Default Shell\n\n\n\n\n"
+	sleep 5
 	chsh -s /usr/bin/zsh
 
-	# ----- Install Fonts -----
+	# Install Fonts
+	echo -e "\n\n\n\n\nInstall Fonts\n\n\n\n\n"
+	sleep 5
 	yes | sudo pacman -S powerline-fonts
 	yes | sudo pacman -S adobe-source-han-sans-kr-fonts
 	yes | sudo pacman -S adobe-source-han-serif-kr-fonts
 	yes | sudo pacman -S ttf-hack-nerd
 
-	# ----- NeoVim 플러그인 사용을 위한 것 -----
+	# Instal Node.js and yarn
+	echo -e "\n\n\n\n\nInstal Node.js and yarn\n\n\n\n\n"
+	sleep 5
 	yes | sudo pacman -S nodejs
 	yes | sudo pacman -S yarn
 	yes | sudo pacman -S npm
 
-else
-	echo "other"
+	# Run install_zsh_plugins Function
+	install_zsh_plugins
+
+	exit 0
 fi
-
-# ------- Install ZSH -------
-# | Powerlevel10k 설치       |
-# | zsh 구문 강조 플러그인   |
-# | zsh 자동 제안 플러그인   |
-# ----------------------------
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# ----- vim-plug -----
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Exit the script
 exit 0
