774a24e2c43130ee3ef71c0fd00dee584e1e70ac
2daeeun <leedaeeun@yahoo.com>
2024-12-23 13:17:33 +0900

    [nvim] LSP 비활성화 플러그인

---
+++ home/.config/nvim/lua/plugins/lsp-disable.lua
@@ -0,0 +1,13 @@
+return {
+  "neovim/nvim-lspconfig",
+  enabled = function()
+    -- 현재 작업 디렉토리 확인
+    local cwd = vim.fn.getcwd()
+    -- 특정 디렉토리와 비교
+    -- if cwd:find("키워드") then
+    if cwd:find("kernel_study") then
+      return false -- LSP 비활성화
+    end
+    return true -- 그 외에는 활성화
+  end,
+}
