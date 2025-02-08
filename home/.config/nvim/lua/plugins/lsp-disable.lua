5c20c55078f2e9b90abcbfe9704866f0252831c6
2daeeun <2daeeun.dev@gmail.com>
2025-02-08 21:04:29 +0900

    [nvim] lsp-disable 플러그인 수정

--- home/.config/nvim/lua/plugins/lsp-disable.lua
+++ home/.config/nvim/lua/plugins/lsp-disable.lua
@@ -2,13 +2,23 @@
 return {
   "neovim/nvim-lspconfig",
   enabled = function()
-    -- 현재 작업 디렉토리 확인
-    local cwd = vim.fn.getcwd()
-    -- 특정 디렉토리와 비교
-    -- if cwd:find("키워드") then
-    if cwd:find("kernel_study") then
-      return false -- LSP 비활성화
+    -- 현재 작업 디렉토리에서 마지막 폴더명 또는 파일명을 가져오기
+    local cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ":t") -- 현재 디렉토리명 가져오기
+    local filename = vim.fn.expand("%:t") -- 현재 열려 있는 파일명 가져오기
+
+    -- LSP를 비활성화할 키워드 목록
+    local disabled_keywords = {
+      "kernel_study",
+      "skiplist.h",
+    }
+
+    -- 현재 디렉토리명 또는 파일명에 키워드가 포함되는지 확인
+    for _, keyword in ipairs(disabled_keywords) do
+      if cwd:find(keyword, 1, true) or filename:find(keyword, 1, true) then
+        return false -- LSP 비활성화
+      end
     end
-    return true -- 그 외에는 활성화
+
+    return true -- 그 외에는 LSP 활성화
   end,
 }
