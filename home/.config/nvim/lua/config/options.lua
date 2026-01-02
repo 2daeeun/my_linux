9b7382f0a90ef29e0293068da1eab2c4fd7d546e
2daeeun <2daeeun.dev@gmail.com>
2026-01-02 13:54:57 +0900

    [nvim] folding 플러그인 관련 작업 (conceallevel을 0으로 설정)

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 마크다운 문법 기호를 숨기지 않도록 conceallevel을 0으로 설정
+-- (conceallevel: 특정 텍스트 숨김(conceal) 기능을 비활성화하는 설정)
+local opt = vim.opt
+opt.conceallevel = 0
