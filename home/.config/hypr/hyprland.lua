106e914d84faa19996343418bf8708c64d1b539a
2daeeun <2daeeun.dev@gmail.com>
2026-08-21 15:15:37 +0900

    [hyprland] 창 border 색상 및 불투명도 조절

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -17,15 +17,15 @@
 -- hyprctl monitors
 
 -- 일반
--- hl.monitor({ output = "eDP-1", mode = "2160x1350@59.74", position = "4892x1680", scale = 1.0 }) -- 내장 모니터
+hl.monitor({ output = "eDP-1", mode = "2160x1350@59.74", position = "4892x1680", scale = 1.0 }) -- 내장 모니터
 
 -- === Monitors (집) ===
--- hl.monitor({ output = "eDP-1", mode = "2160x1350@59.74", position = "4892x1680", scale = 1.0 })                -- 내장 모니터
--- hl.monitor({ output = "DP-5",  mode = "1920x1080@60.0",  position = "4892x600",  scale = 1.0 })                -- 외장 모니터 1 (중앙)
--- hl.monitor({ output = "DP-6",  mode = "1920x1080@60.32", position = "3812x600",  scale = 1.0, transform = 1 }) -- 외장 모니터 2 (왼쪽)
--- hl.workspace_rule({ workspace = "3", monitor = "eDP-1", default = true }) -- 워크스페이스 3
--- hl.workspace_rule({ workspace = "2", monitor = "DP-5",  default = true }) -- 워크스페이스 1
--- hl.workspace_rule({ workspace = "1", monitor = "DP-6",  default = true }) -- 워크스페이스 2
+-- hl.monitor({ output = "eDP-1", mode = "2160x1350@59.74", position = "4892x1680", scale = 1.0 }) -- 내장 모니터
+-- hl.monitor({ output = "DP-5", mode = "1920x1080@60.0", position = "4892x600", scale = 1.0 }) -- 외장 모니터 1 (중앙)
+-- hl.monitor({ output = "DP-6", mode = "1920x1080@60.32", position = "3812x600", scale = 1.0, transform = 1 }) -- 외장 모니터 2 (왼쪽)
+-- hl.workspace_rule({ workspace = "10", monitor = "eDP-1", default = true }) -- 워크스페이스 10
+-- hl.workspace_rule({ workspace = "2", monitor = "DP-5", default = true }) -- 워크스페이스 1
+-- hl.workspace_rule({ workspace = "1", monitor = "DP-6", default = true }) -- 워크스페이스 2
 
 -- === Monitors (델 워크스테이션) ===
 -- hl.monitor({ output = "DP-1", mode = "1920x1080@60.0",  position = "4892x0", scale = 1.0 })                -- 외장 모니터 1 (중앙)
@@ -39,20 +39,20 @@
 -- })
 
 -- === Monitors (연구실 1) ===
--- hl.monitor({ output = "eDP-1", mode = "2160x1350@59.74", position = "4892x0",    scale = 1.0 })                -- 내장 모니터
--- hl.monitor({ output = "DP-5",  mode = "2560x1440@29.99", position = "4892x1350", scale = 1.0 })                -- 외장 모니터 1 (중앙)
--- hl.monitor({ output = "DP-6",  mode = "2560x1440@29.99", position = "3452x1350", scale = 1.0, transform = 1 }) -- 외장 모니터 2 (왼쪽)
--- hl.workspace_rule({ workspace = "9", monitor = "eDP-1", default = true }) -- 워크스페이스 10
--- hl.workspace_rule({ workspace = "2", monitor = "DP-5",  default = true }) -- 워크스페이스 1
--- hl.workspace_rule({ workspace = "1", monitor = "DP-6",  default = true }) -- 워크스페이스 2
+-- hl.monitor({ output = "eDP-1", mode = "2160x1350@59.74", position = "4892x0", scale = 1.0 }) -- 내장 모니터
+-- hl.monitor({ output = "DP-5", mode = "2560x1440@29.99", position = "4892x1350", scale = 1.0 }) -- 외장 모니터 1 (중앙)
+-- hl.monitor({ output = "DP-6", mode = "2560x1440@29.99", position = "3452x1350", scale = 1.0, transform = 1 }) -- 외장 모니터 2 (왼쪽)
+-- hl.workspace_rule({ workspace = "10", monitor = "eDP-1", default = true }) -- 워크스페이스 10
+-- hl.workspace_rule({ workspace = "2", monitor = "DP-5", default = true }) -- 워크스페이스 1
+-- hl.workspace_rule({ workspace = "1", monitor = "DP-6", default = true }) -- 워크스페이스 2
 
 -- === Monitors (연구실 2) ===
-hl.monitor({ output = "DP-5", mode = "2560x1440@29.99", position = "4892x0", scale = 1.0 }) -- 외장 모니터 1 (중앙)
-hl.monitor({ output = "DP-6", mode = "2560x1440@29.99", position = "3452x0", scale = 1.0, transform = 1 }) -- 외장 모니터 2 (왼쪽)
-hl.monitor({ output = "eDP-1", mode = "2160x1350@59.74", position = "4892x1440", scale = 1.0 }) -- 내장 모니터
-hl.workspace_rule({ workspace = "0", monitor = "eDP-1", default = true }) -- 워크스페이스 10
-hl.workspace_rule({ workspace = "1", monitor = "DP-6", default = true }) -- 워크스페이스 1
-hl.workspace_rule({ workspace = "2", monitor = "DP-5", default = true }) -- 워크스페이스 2
+-- hl.monitor({ output = "DP-5", mode = "2560x1440@29.99", position = "4892x0", scale = 1.0 }) -- 외장 모니터 1 (중앙)
+-- hl.monitor({ output = "DP-6", mode = "2560x1440@29.99", position = "3452x0", scale = 1.0, transform = 1 }) -- 외장 모니터 2 (왼쪽)
+-- hl.monitor({ output = "eDP-1", mode = "2160x1350@59.74", position = "4892x1440", scale = 1.0 }) -- 내장 모니터
+-- hl.workspace_rule({ workspace = "10", monitor = "eDP-1", default = true }) -- 워크스페이스 10
+-- hl.workspace_rule({ workspace = "1", monitor = "DP-6", default = true }) -- 워크스페이스 1
+-- hl.workspace_rule({ workspace = "2", monitor = "DP-5", default = true }) -- 워크스페이스 2
 
 ---------------------
 ---- 프로그램 설정 ----
@@ -128,7 +128,7 @@ hl.config({
 		gaps_in = 5,
 		gaps_out = 10,
 
-		border_size = 2,
+		border_size = 5,
 
 		col = {
 			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
@@ -150,7 +150,7 @@ hl.config({
 
 		-- 포커스를 받은 창과 받지 않은 창의 투명도 설정
 		active_opacity = 1.0,
-		inactive_opacity = 1.0,
+		inactive_opacity = 0.95,
 
 		shadow = {
 			enabled = true,
